f3391406af87509ae3f2576a5a756a159dac959a
iiivanpopov <iiivanpopov999@gmail.com>
2024-12-16 20:40:03 +0200

    chore: add batch script to update the app

---
+++ update.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo Executing git pull...
+git pull
+if %errorlevel% neq 0 (
+    echo Error executing git pull.
+    pause
+    exit /b %errorlevel%
+)
+
+echo git pull completed successfully.
+
+echo Executing docker-compose build...
+docker-compose build
+if %errorlevel% neq 0 (
+    echo Error executing docker-compose build.
+    pause
+    exit /b %errorlevel%
+)
+
+echo docker-compose build completed successfully.
+
+pause
