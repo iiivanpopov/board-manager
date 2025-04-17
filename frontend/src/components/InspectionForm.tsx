661d140ceb9a0435d6e03ba6bb41cda0e1e74ada
iiivanpopov <iiivanpopov999@gmail.com>
2025-04-17 19:02:50 +0300

    feat: 'other' defect type

--- frontend/src/components/InspectionForm.tsx
+++ frontend/src/components/InspectionForm.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { formatDate } from '../helpers/date'
 import { Context } from '../main'
@@ -9,90 +9,91 @@ import { DateInput } from './DateInput'
 import { Dropdown } from './Dropdown'
 import { Input } from './Input'
 
-export const InspectionForm: React.FC<{ className?: string }> = observer(
-	({ className }) => {
-		const { store } = useContext(Context)
+export const InspectionForm: React.FC<{ className?: string }> = observer(({ className }) => {
+	const { store } = useContext(Context)
 
-		const [date, setDate] = useState<string>(formatDate(new Date()))
-		const [product, setProduct] = useState<string>('')
-		const [defectType, setDefectType] = useState<string>('')
-		const [defect, setDefect] = useState<string>('')
-		const [quantity, setQuantity] = useState<string>('1')
-		const [idMode, setIdMode] = useState<boolean>(false)
+	const [date, setDate] = useState<string>(formatDate(new Date()))
+	const [product, setProduct] = useState<string>('')
+	const [defectType, setDefectType] = useState<string>('')
+	const [defect, setDefect] = useState<string>('')
+	const [quantity, setQuantity] = useState<string>('1')
+	const [idMode, setIdMode] = useState<boolean>(false)
 
-		const weekNumber = new Date(date).getWeek()
+	const weekNumber = new Date(date).getWeek()
 
-		const products = store.options.products
-		const defects = store.options.defects
-		const defectTypes = store.options.defectTypes
+	const products = store.options.products
+	const defects = store.options.defects
+	const defectTypes = [...store.options.defectTypes, 'Other']
 
-		const isFormValid = product && defectType && defect && date
+	const isFormValid = product && defectType && defect && date
 
-		const handleSave = async () => {
-			try {
-				await store.saveInspection(
-					{
-						date: new Date(date),
-						week: weekNumber,
-						inspector: store.user.fullName,
-						product,
-						defectType,
-						defect,
-						worker: +store.worker,
-					},
-					+quantity
-				)
-				setQuantity('1')
-			} catch (error) {
-				console.error('Error saving inspection:', error)
-			}
+	const prevDefectTypeRef = useRef(defectType)
+
+	useEffect(() => {
+		if (prevDefectTypeRef.current.toLowerCase() == 'other' && defectType.toLowerCase() != 'other') {
+			setDefect('')
 		}
+		prevDefectTypeRef.current = defectType
+	}, [defectType])
 
-		return (
-			<div
-				className={twMerge(
-					'grid grid-rows-6 items-center h-full w-full lg:w-3/4 gap-4 p-5',
-					className
-				)}
-			>
-				<DateInput
-					label={`Week: ${weekNumber}`}
-					onChange={setDate}
-					selected={date}
-				/>
+	const handleSave = async () => {
+		try {
+			await store.saveInspection(
+				{
+					date: new Date(date),
+					week: weekNumber,
+					inspector: store.user.fullName,
+					product,
+					defectType,
+					defect,
+					worker: +store.worker,
+				},
+				+quantity
+			)
+			setQuantity('1')
+		} catch (error) {
+			console.error('Error saving inspection:', error)
+		}
+	}
 
-				<div className='flex gap-x-8'>
-					<Dropdown
-						options={
-							idMode ? products.map(prod => prod.split(' ')[0]) : products
-						}
-						placeholder='Enter a product'
-						onChange={setProduct}
-						value={product}
-					/>
-					<Checkbox checked={idMode} onChange={setIdMode} />
-				</div>
+	return (
+		<div
+			className={twMerge(
+				'grid grid-rows-6 items-center h-full w-full lg:w-3/4 gap-4 p-5',
+				className
+			)}
+		>
+			<DateInput label={`Week: ${weekNumber}`} onChange={setDate} selected={date} />
+
+			<div className='flex gap-x-8'>
 				<Dropdown
-					options={defectTypes}
-					placeholder='Enter a defect type'
-					onChange={setDefectType}
-					value={defectType}
+					options={idMode ? products.map(prod => prod.split(' ')[0]) : products}
+					placeholder='Enter a product'
+					onChange={setProduct}
+					value={product}
 				/>
+				<Checkbox checked={idMode} onChange={setIdMode} />
+			</div>
+			<Dropdown
+				options={defectTypes}
+				placeholder='Enter a defect type'
+				onChange={setDefectType}
+				value={defectType}
+			/>
+			{defectType.toLocaleLowerCase() == 'other' ? (
+				<Input value={defect} onChange={setDefect} placeholder='Enter a defect' />
+			) : (
 				<Dropdown
 					options={defects}
 					placeholder='Enter a defect'
 					onChange={setDefect}
 					value={defect}
 				/>
-				<Input
-					value={quantity}
-					onChange={setQuantity}
-					placeholder='Enter a quantity'
-				/>
-				<Button disabled={!isFormValid} color='green' onClick={handleSave}>
-					Save
-				</Button>
-			</div>
-		)
-	}
-)
+			)}
+			<Input value={quantity} onChange={setQuantity} placeholder='Enter a quantity' />
+			<Button disabled={!isFormValid} color='green' onClick={handleSave}>
+				Save
+			</Button>
+		</div>
+	)
+})
